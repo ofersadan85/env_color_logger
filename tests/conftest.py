3b14ba47cbb7e4ce1f650ef0e3198d3bf82d6d2c
Ofer Sadan <ofersadan85@gmail.com>
2023-04-13 15:33:19 +0300

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+# conftest.py
+
+# Add shared pytest fixtures, hooks, or plugins in this file as needed.
---
+++ tests/test_envlogger.py
@@ -0,0 +1,40 @@
+import logging
+import tempfile
+
+from envlogger import EnvLogger
+
+
+def test_env_logger_default_values(monkeypatch):
+    monkeypatch.delenv("LOG_FILE_ROTATE", raising=False)
+    monkeypatch.delenv("LOG_FILE_PATH", raising=False)
+    monkeypatch.delenv("LOG_FILE_SIZE", raising=False)
+    monkeypatch.delenv("LOG_FORMAT", raising=False)
+    monkeypatch.delenv("LOG_LEVEL", raising=False)
+    monkeypatch.delenv("LOG_COLOR", raising=False)
+
+    logger = EnvLogger("test_logger")
+
+    assert logger.level == logging.INFO
+    assert len(logger.handlers) == 1
+    assert isinstance(logger.handlers[0], logging.StreamHandler)
+
+
+def test_env_logger_with_file_rotate(monkeypatch):
+    with tempfile.TemporaryDirectory() as tempdir:
+        monkeypatch.setenv("LOG_FILE_ROTATE", "3")
+        monkeypatch.setenv("LOG_FILE_PATH", tempdir)
+        monkeypatch.setenv("LOG_FILE_SIZE", "1024")
+
+        logger = EnvLogger("test_logger")
+
+        assert logger.level == logging.INFO
+        assert len(logger.handlers) == 2
+        assert isinstance(logger.handlers[1], logging.handlers.RotatingFileHandler)
+
+        file_handler = logger.handlers[1]
+        assert file_handler.maxBytes == 1024
+        assert file_handler.backupCount == 3
+
+        # Close the file handler to avoid locking the file(s) on Windows
+        for handler in logger.handlers:
+            handler.close()
